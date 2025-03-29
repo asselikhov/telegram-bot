b30af1c2948caabcf819a3d255ef72e96100cd0e
asselikhov <selikhov58@yandex.ru>
2025-03-29 22:51:49 +0300

    Bot 2.2.4

--- src/bot/actions/organization.js
+++ src/bot/actions/organization.js
@@ -10,6 +10,14 @@ async function showOrganizationSelection(ctx, userId) {
 }
 
 module.exports = (bot) => {
+    bot.action(/select_organization_(\d+)/, async (ctx) => {
+        const userId = ctx.from.id.toString();
+        ctx.state.userStates[userId] = { step: 'enterFullName', selectedObjects: [], report: {}, messageIds: [] };
+        const message = await ctx.reply('Введите ваше ФИО:');
+        ctx.state.userStates[userId].messageIds.push(message.message_id);
+        console.log(`Шаг enterFullName установлен для userId ${userId}. State:`, ctx.state.userStates[userId]);
+    });
+
     bot.on('text', async (ctx) => {
         const userId = ctx.from.id.toString();
         console.log(`Получен текст от userId ${userId}: "${ctx.message.text}"`);
@@ -26,15 +34,6 @@ module.exports = (bot) => {
             console.log(`Шаг не enterFullName или state отсутствует для userId ${userId}`);
         }
     });
-
-    bot.action(/select_organization_(\d+)/, async (ctx) => {
-        const userId = ctx.from.id.toString();
-        ctx.state.userStates[userId] = { step: 'enterFullName', selectedObjects: [], report: {}, messageIds: [] };
-        const message = await ctx.reply('Введите ваше ФИО:');
-        ctx.state.userStates[userId].messageIds.push(message.message_id);
-        console.log(`Шаг enterFullName установлен для userId ${userId}. State:`, ctx.state.userStates[userId]);
-    });
 };
 
-// Экспортируем функцию отдельно
 module.exports.showOrganizationSelection = showOrganizationSelection;
