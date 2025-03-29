d2dadf04040083ba96c8f7ddcee0f886f588c3a0
asselikhov <selikhov58@yandex.ru>
2025-03-29 23:36:19 +0300

    Remove menu dependency with placeholder

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -79,7 +79,9 @@ bot.on('text', async (ctx) => {
       state.messageIds = [message.message_id];
       console.log(`Новый пользователь ${userId} начал регистрацию с выбора объектов`);
     } else if (users[userId].isApproved) {
-      await require('./menu').showMainMenu(ctx);
+      // Заглушка вместо showMainMenu
+      await ctx.reply('Вы подтверждены! Главное меню скоро будет доступно.');
+      console.log(`Подтверждённый пользователь ${userId} получил заглушку меню`);
     } else {
       const user = users[userId];
       state.messageIds = [];
