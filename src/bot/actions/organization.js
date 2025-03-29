b8421afdf28d3309b03ef6eb997a115d60b25807
asselikhov <selikhov58@yandex.ru>
2025-03-29 23:29:01 +0300

    Bot 2.2.7

--- src/bot/actions/organization.js
+++ src/bot/actions/organization.js
@@ -1,22 +1,11 @@
 const { Markup } = require('telegraf');
-
-async function showOrganizationSelection(ctx, userId) {
-    const buttons = [
-        [Markup.button.callback('Тестовая организация', 'select_organization_0')],
-        [Markup.button.callback('Ввести свою', 'custom_organization')]
-    ];
-    const message = await ctx.reply('Выберите вашу организацию:', Markup.inlineKeyboard(buttons));
-    ctx.state.userStates[userId].messageIds = [message.message_id];
-}
+const { showOrganizationSelection } = require('../actions/organization');
 
 module.exports = (bot) => {
-    bot.action(/select_organization_(\d+)/, async (ctx) => {
+    bot.command('start', async (ctx) => {
         const userId = ctx.from.id.toString();
-        ctx.state.userStates[userId] = { step: 'enterFullName', selectedObjects: [], report: {}, messageIds: [] };
-        const message = await ctx.reply('Введите ваше ФИО:');
-        ctx.state.userStates[userId].messageIds.push(message.message_id);
-        console.log(`Шаг enterFullName установлен для userId ${userId}. State:`, ctx.state.userStates[userId]);
+        console.log(`[Start] Команда /start от userId ${userId}`);
+        await ctx.reply('Начинаем регистрацию!');
+        await showOrganizationSelection(ctx, userId);
     });
 };
-
-module.exports.showOrganizationSelection = showOrganizationSelection;
