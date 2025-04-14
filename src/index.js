919c042c40d827545419dc644bd534e4b732ea6a
asselikhov <selikhov58@yandex.ru>
2025-04-14 12:15:16 +0300

    Bot 4.2.8

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const { initializeDatabase } = require('./database/db');
+require('dotenv').config();
+const { connectMongo } = require('./config/mongoConfig');
 const bot = require('./bot/bot');
 require('./server/server');
 
-initializeDatabase();
+async function startApp() {
+    try {
+        console.log('Попытка подключения к MongoDB...');
+        await connectMongo();
+        console.log('MongoDB подключен');
+        console.log('Запуск приложения...');
+        // Бот и сервер запускаются через require('./bot/bot') и require('./server/server')
+    } catch (err) {
+        console.error('Ошибка при запуске приложения:', err);
+        process.exit(1);
+    }
+}
+
+startApp();
 
 process.on('uncaughtException', (err) => {
+    console.error('Неперехваченная ошибка:', err);
     process.exit(1);
 });
